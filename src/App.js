a26034d75f55e57a0eca82ae0cc4da724bd7e024
AndriiPopkov <132999234+AndriiPopkov@users.noreply.github.com>
2024-03-01 15:38:34 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Numbers from './components/Numbers/Numbers';
+
+function App() {
+  return (
+    <div className="wrapper">
+    <Numbers/>
+    </div>
+  );
+}
+
+export default App;
